4aea7324c852bff484817acac6641664d6b949de
Fredrick <fredrickcuche@gmail.com>
2022-09-14 13:08:35 -0700

    hard

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+/**
+ * print_alphabet - print_alphabet
+ * Does my stuff explained.
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
+
+
